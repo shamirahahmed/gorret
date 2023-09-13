7b545a9946360872e8daabec1b2ad01e53e33ce8
shamirahahmed <141908439+shamirahahmed@users.noreply.github.com>
2023-09-13 14:07:15 +0300

    Add files via upload

---
+++ gorret assigment.py
@@ -0,0 +1,22 @@
+#varaiables
+x=1 #integer
+y=3.45 #float
+z="Victy"#string
+print(x,y,z)
+
+if x>1: y>3.45;
+print(True or False)
+print (x)
+print (y)
+print (z)
+#booleans
+
+Bright=20
+Dull=45
+#if Bright>Dull;
+print(True)
+
+#strings
+pal_1="Allah"
+pal_1="Alhamudhulila"
+print(pal_1, pal_1)
